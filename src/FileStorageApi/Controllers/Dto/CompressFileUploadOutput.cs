5ffa7f577116ccc5896cc8c6bd26b81f02b19ad2
zzq <zzq@eson101.com>
2019-05-07 10:27:46 +0800

    #3987 上传压缩包，提供假接口

---
+++ src/FileStorageApi/Controllers/Dto/CompressFileUploadOutput.cs
@@ -0,0 +1,29 @@
+﻿
+namespace FileStorageApi.Controllers.Dto
+{
+    /// <summary>
+    /// 压缩包上传文件返回参数
+    /// </summary>
+    public sealed class CompressFileUploadOutput
+    {
+        /// <summary>
+        /// 文件存储路径
+        /// </summary>
+        public string FileUrl { get; set; }
+
+        /// <summary>
+        /// 文件名
+        /// </summary>
+        public string FileName { get; set; }
+
+        /// <summary>
+        /// 文件的Md5
+        /// </summary>
+        public string FileMd5 { get; set; }
+
+        /// <summary>
+        /// 文件的相对路径
+        /// </summary>
+        public string FolderPath { get; set; }
+    }
+}
